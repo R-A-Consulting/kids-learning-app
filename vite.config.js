e46d238f9e56e9bb7849b05ee7125c2bb1472134
Rishabh Rathore <xerycks@gmail.com>
2025-09-17 14:32:07 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -24,26 +24,26 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    port: 5174,
-    proxy: {
-      '/api/v1': {
-        target: 'https://q3rjb9vf-3000.inc1.devtunnels.ms',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
-  },
+  // server: {
+  //   port: 5174,
+  //   proxy: {
+  //     '/api/v1': {
+  //       target: 'https://q3rjb9vf-3000.inc1.devtunnels.ms',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path,
+  //       configure: (proxy, _options) => {
+  //         proxy.on('error', (err, _req, _res) => {
+  //           console.log('proxy error', err);
+  //         });
+  //         proxy.on('proxyReq', (proxyReq, req, _res) => {
+  //           console.log('Sending Request to the Target:', req.method, req.url);
+  //         });
+  //         proxy.on('proxyRes', (proxyRes, req, _res) => {
+  //           console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+  //         });
+  //       },
+  //     }
+  //   }
+  // },
 });
